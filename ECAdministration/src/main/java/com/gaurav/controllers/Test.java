6e3a9b042f3c9384fc5aae198602cb1809002a97
Awasthi <Awasthi@Awasthi>
2017-06-09 12:32:12 +0530

    checkin files

--- ECAdministration/src/main/java/com/gaurav/controllers/Test.java
+++ ECAdministration/src/main/java/com/gaurav/controllers/Test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 	@Controller
-	@RequestMapping("/hello")
+	/*@RequestMapping("/hello")*/
 	public class Test {
 		   @RequestMapping(method = RequestMethod.GET)
 		   public String printHello(ModelMap model) 
--- ECAdministration/src/main/java/com/gaurav/controllers/TestController.java
+++ ECAdministration/src/main/java/com/gaurav/controllers/TestController.java
@@ -3,10 +3,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
 @Controller 
-@RequestMapping("/hello1") 
+/*@RequestMapping("/hello1")*/ 
 public class TestController {
 
-	    @RequestMapping("/hello")  
+	    /*@RequestMapping("/hello") */ 
 	    public ModelAndView helloWorld() {  
 	    	System.out.println("control comes here");
 	        String message = "HELLO SPRING MVC";  
